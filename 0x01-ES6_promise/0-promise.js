697ce00b9f94d12017a60602e9f72c8bad01b324
lilwm <lmiiri95@gmail.com>
2023-04-19 18:48:21 +0000

    initial commit

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,22 @@
+// get-response.js
+
+function getResponseFromAPI(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject(new Error(xhr.statusText));
+      }
+    };
+    xhr.onerror = () => {
+      reject(new Error('Network error'));
+    };
+    xhr.send();
+  });
+}
+
+module.exports = getResponseFromAPI;
+

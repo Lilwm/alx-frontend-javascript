ba65c35ca8ad1439dbd6346cee6e07c6e45d8e9e
lilwm <lmiiri95@gmail.com>
2023-04-20 20:11:36 +0000

    hot fix

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,12 +2,10 @@
 // returns array of 3 ClassRoom objects sizes sizes 19, 20, and 34
 import ClassRoom  from './0-classroom'
 
-function initializeRooms(){
-    return[ 
-    new ClassRoom[19],
-    new ClassRoom[20],
-    new ClassRoom[34]]
-}
-
+function initializeRooms() {
+    const sizes = [19, 20, 34];
+    const rooms = sizes.map(size => new ClassRoom(size));
+    return rooms;
+  }
 const rooms = initializeRooms();
 console.log(rooms);

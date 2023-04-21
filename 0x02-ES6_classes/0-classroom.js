9f39ded54ff7fbae31c81d6df3828f5f4cbb773d
lilwm <lmiiri95@gmail.com>
2023-04-21 06:14:37 +0000

    fixed lint issues

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,8 +1,7 @@
 //  class accepts one attribute in its constructor, and assigns it to a private instance variable 
 
-export default class ClassRoom{
-    constructor (maxStudentsSize){
-        this._maxStudentsSize = maxStudentsSize
-    }
-    
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
